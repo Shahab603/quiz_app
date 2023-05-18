86dd5ce60e3a1b8ff54e6e3214869bbb5e8c152d
Shahab <shahab.nadeem@gmail.com>
2023-05-18 14:10:39 +0500

    07 - Another method of passing values and funtions using Ternary Method

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -10,41 +10,34 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
 
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
-
-  void switchScreen(){
-    
+  void switchScreen() {
     setState(() {
-      activeScreen = const QuestionScreen();
+      activeScreen = 'question-screen';
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        home: Scaffold(
-            body: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color.fromARGB(255, 78, 13, 151),
-                      Color.fromARGB(255, 107, 15, 168),
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: activeScreen == 'start-screen'
+                ? StartScreen(switchScreen)
+                : const QuestionScreen()
                 ),
-                child: activeScreen
-                ),
-                ),
-                );
+      ),
+    );
   }
-  }
+}
