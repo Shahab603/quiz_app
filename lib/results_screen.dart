86b3b08845d9f6befe491c248852e009e15fd938
Shahab <shahab.nadeem@gmail.com>
2023-05-30 22:47:32 +0500

    Introduction to for loops in list

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.choosenAnswer});
 
   final List<String> choosenAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choosenAnswer.length; i++) {
+      summary.add({
+        'question-index': i,
+        'question': questions[i].text,
+        'correct-answer': questions[i].answers[0],
+        'choosen-answer': choosenAnswer[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
